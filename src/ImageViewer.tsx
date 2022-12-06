a9fa8d344d8b949e1764457b337360b4987de5e3
Roy QIU <karoyqiu@gmail.com>
2022-12-06 19:37:08 +0800

    重构恢复功能。

--- src/ImageViewer.tsx
+++ src/ImageViewer.tsx
@@ -7,28 +7,35 @@ import windowSize from './entities/windowSize';
 type ImageViewerProps = {
   dir: string;
   images: string[];
+  pos: number;
 };
 
 export default function ImageViewer(props: ImageViewerProps) {
-  const { dir, images } = props;
+  const { dir, images, pos } = props;
   const [ratio, setRatio] = React.useState(0);
   const wsize = useEntity(windowSize);
+  const ref = React.useRef<FixedSizeList>(null);
 
   React.useEffect(() => {
     const img = new Image();
-    img.onload = () => setRatio(img.naturalHeight / img.naturalWidth);
+    img.onload = () => {
+      setRatio(img.naturalHeight / img.naturalWidth);
+      ref.current?.scrollTo(pos);
+    };
     img.src = `https://imgaaa.localhost/${dir}/${images[0]}`;
-  }, [dir, images[0]]);
+  }, [dir, images[0], pos]);
 
   return (
     <AutoResizer>
       {({ width, height }) => (
         <FixedSizeList
+          ref={ref}
           width={width}
           height={height}
           itemCount={images.length}
           itemSize={wsize.width * ratio}
           overscanCount={3}
+          onScroll={({ scrollOffset }) => localStorage.setItem('pos', `${scrollOffset}`)}
         >
           {({ style, index }) => (
             <img
